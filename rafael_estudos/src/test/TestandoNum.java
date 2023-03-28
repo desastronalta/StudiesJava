58578842e3d51097a60516c63df0b0e81287fdd5
Desastronalta <83319735+desastronalta@users.noreply.github.com>
2023-03-28 18:42:55 -0300

    testing sum number : p

---
+++ rafael_estudos/src/test/TestandoNum.java
@@ -0,0 +1,13 @@
+package test;
+
+public class TestandoNum {
+	public static void main(String[]args) {
+		int i = 1;
+		int soma = 0;
+		while(i <= 1000) {
+			soma += i;
+			i++;
+		}
+		System.out.println("a soma é: " + soma);
+	}
+}
